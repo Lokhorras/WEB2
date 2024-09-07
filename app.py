7b044d87e1d220b82b55b542971a1cfc359351bd
daniil mirakin <danilmir0253@gmail.com>
2024-09-07 22:33:34 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ def not_found(err):
 
 
 
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html> 
         <html> 
             <body> 
                 <h1>web-сервер на flask</h1> 
-                <a href="/author">author</a> 
+                <a href="/lab1/author">author</a> 
             </body> 
         </html>""", 200, {
                 'X-Server': 'sample',
@@ -22,7 +22,7 @@ def web():
         }
 
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Миракин Д.В."
     group = "ФБИ-22"
@@ -34,7 +34,7 @@ def author():
                 <p>Студент: """ + name + """</p>
                 <p>Группа: """ + group + """</p>  
                 <p>Факультет: """ + faculty + """</p>  
-                <a href="/web">web</a>
+                <a href="/lab1/web">web</a>
             </body> 
         </html>"""
         
@@ -83,9 +83,9 @@ def clear():
 '''
 
 
-@app.route('/info')
+@app.route('/lab1/info')
 def info():
-    return redirect('/author')
+    return redirect('/lab1/info')
 
 @app.route('/lab1/created')
 def created():
