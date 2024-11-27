c65de9180021247aa072baf9f226588d0ad17cee
mir <student@FB.LOCAL>
2024-11-27 16:07:52 +0700

    правки

--- lab6.py
+++ lab6.py
@@ -9,14 +9,29 @@ lab6 = Blueprint('lab6', __name__)
 
 
 offices = []
-conn = psycopg2.connect(
-    host = '127.0.0.1',
-    database = 'one',
-    user = 'one',
-    password = '123'
-    )
+def db_connect():
+    if current_app.config['DB_TYPE'] == 'postgres':
+        conn = psycopg2.connect(
+            host = '127.0.0.1',
+            database = 'one',
+            user = 'one',
+            password = '123'
+        )
+        cur = conn.cursor(cursor_factory = RealDictCursor)
+    else:
+        dir_path = path.dirname(path.realpath(__file__))
+        db_path = path.join(dir_path, "database.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        
+    return conn, cur
 
-cur = conn.cursor()
+conn, cur = db_connect()
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 cur.execute("DELETE FROM offices;")
