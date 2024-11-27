f47a3d91a7b87a9fad12a9146869cec4918f87b9
mir <student@FB.LOCAL>
2024-11-27 17:40:54 +0700

    правки222

--- lab6.py
+++ lab6.py
@@ -40,6 +40,7 @@ def add():
         {'number': 4, 'tenant': ''},
         {'number': 5, 'tenant': ''}
     ]
+    cur.execute('CREATE TABLE offices(number INT, tenant VARCHAR(255))')
     conn, cur = db_connect()
     for office in offices:
         cur.execute("INSERT INTO offices (number, tenant) VALUES (%s, %s);", (office['number'], office['tenant']))
